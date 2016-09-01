8b7cbc2510cf53b322ac33205729799fc9702a7e
Benjamin Kies <ben@joingrouper.com>
2016-09-01 19:01:09 -0400

    Get random challenges to work with server :)
    Hurray!

--- app/providers/challenge/challenge.ts
+++ app/providers/challenge/challenge.ts
@@ -14,10 +14,21 @@ import { CHALLENGES_JSON } from './mock-challenges'
 */
 @Injectable()
 export class ChallengeProvider {
+  baseUrl = 'http://localhost:3000/api/'
 
   constructor(private http: Http) {}
 
   getRandom(level) {
+    return this.http.get(this.baseUrl + "challenges/random" + "?level=" + level)
+      .map(res => new Challenge(
+          res.json()['id'],
+          res.json()['content'],
+          res.json()['level']
+        )
+      ).catch(this.handleError);
+  }
+
+  getRandomMock(level) {
     return Observable.create(observer => {
       observer.next(
         CHALLENGES_JSON.map(challenge_json => new Challenge(
@@ -48,5 +59,10 @@ export class ChallengeProvider {
       observer.next(console.log("Saving response:", response))
       observer.complete();
     })
+  }
+
+  handleError(error) {
+      console.error(error);
+      return Observable.throw(error.json().error || 'Server error');
   }
 }
